70b54f1874712b0c9bac219afb459aa1582dcb99
Önder Sümer <ondersumer07@gmail.com>
2024-03-07 10:44:39 +0300

    add blogCard image and update itemNumber

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -2,7 +2,7 @@
 	import CardWimageReadTime from '$lib/cardWimageReadTime/cardWimageReadTime.svelte';
 	export let data;
 
-	let itemNumber = 6;
+	let itemNumber = 7;
 </script>
 
 <div class="grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-12">
